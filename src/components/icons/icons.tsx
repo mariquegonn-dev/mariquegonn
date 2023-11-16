311118ffe593e8117c86b10e233b0e27bc0c44d8
mariquegonn <ocatodev@gmail.com>
2023-11-16 17:48:39 -0300

    feat: phosphor icons added and component for icons

---
+++ src/components/icons/icons.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export {
+  House as HouseIcon,
+  FloppyDisk as DisketIcon,
+  CodepenLogo as CodeIcon,
+  EnvelopeSimple as MailIcon,
+  GithubLogo as GithubIcon,
+  LinkedinLogo as LinkedinIcon,
+  WhatsappLogo as WhatsappIcon,
+} from "@phosphor-icons/react";
