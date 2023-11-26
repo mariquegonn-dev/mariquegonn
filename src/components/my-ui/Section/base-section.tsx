537f27caf4a168b5a0ad5a1066cb836e2014f5e1
mariquegonn <ocatodev@gmail.com>
2023-11-26 16:18:14 -0300

    feat: new prop added in base-section component

--- src/components/my-ui/Section/base-section.tsx
+++ src/components/my-ui/Section/base-section.tsx
@@ -1,10 +1,16 @@
+import { cn } from "@/lib/utils";
+import { ClassNameValue } from "tailwind-merge";
+
 type BaseSectionProps = {
   children: React.ReactNode;
+  className?: ClassNameValue;
 };
 
-export const BaseSection = ({ children }: BaseSectionProps) => {
+export const BaseSection = ({ children, className }: BaseSectionProps) => {
   return (
-    <section className="p-5 lg:p-10 lg:first:pt-0 border-b-[1px]">
+    <section
+      className={cn("p-5 lg:p-10 lg:first:pt-0 border-b-[1px]", className)}
+    >
       <div className="max-w-[1440px] mx-auto">{children}</div>
     </section>
   );
